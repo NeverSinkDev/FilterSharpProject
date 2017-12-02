6534e6d44d528fb67bd09f434923a6f4a7c29a91
NeverSink <JevJaku@gmail.com>
2017-12-02 02:48:40 +0100

    Structure Progres

---
+++ FilterSharp/FilterCore/Filter/FilterEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FilterCore
+{
+    public class FilterEntry
+    {
+        public FilterLine FilterLine
+        {
+            get => default(FilterLine);
+            set
+            {
+            }
+        }
+    }
+}
---
+++ FilterSharp/FilterCore/Filter/FilterLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FilterCore
+{
+    public class FilterLine
+    {
+        public FilterValue FilterValue
+        {
+            get => default(FilterValue);
+            set
+            {
+            }
+        }
+    }
+}
